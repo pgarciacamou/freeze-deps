90f9f025309bc4eb982b784c8c81c1101b9b4e6b
Pablo Garcia <pgarciacamou@gmail.com>
2019-07-10 14:57:19 -0700

    implement freezeDeps module

--- src/index.js
+++ src/index.js
@@ -1,16 +1,46 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import jsonfile from 'jsonfile';
+import fs from 'fs';
 
-import styles from './styles.css';
-
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  };
-
-  render() {
-    const { text } = this.props;
-
-    return <div className={styles.test}>Example Component: {text}</div>;
+function fileExists(path) {
+  try {
+    if (fs.existsSync(path)) {
+      return true;
+    }
+  } catch (err) {
+    return false;
   }
+}
+
+export function freezeDeps(jsonPath, lockPath, options = {}) {
+  if (!fileExists(jsonPath)) {
+    throw new Error(
+      `File package.json not found, file path received: ${jsonPath}`
+    );
+  }
+
+  if (!fileExists(lockPath)) {
+    throw new Error(
+      `File package-lock.json not found, file path received: ${lockPath}`
+    );
+  }
+
+  const packageJSON = jsonfile.readFileSync(jsonPath);
+  const packageLock = jsonfile.readFileSync(lockPath);
+  const newJSON = { ...packageJSON };
+
+  for (let dependencyName in packageJSON.dependencies) {
+    const currentDep = packageLock.dependencies[dependencyName];
+
+    // Skip dependencies not found in package-lock because we can't assume
+    // whether the version specified in the package.json is correct or not.
+    if (!currentDep) {
+      continue;
+    }
+
+    newJSON.dependencies[dependencyName] = currentDep.version;
+  }
+
+  jsonfile.writeFile(jsonPath, newJSON, { spaces: 2 }, function(err) {
+    if (err) console.error(err);
+  });
 }
